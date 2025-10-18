a3ffae458ccbc2019cedfc27f237a4918067ce7e
Amir hosein <a.h.mohammadi5@gmail.com>
2025-10-18 17:03:36 +0330

    create docs folder

---
+++ docs/_next/static/chunks/e88971ec81883631.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/b7b1e8984d01cf8d.js",
+  "static/chunks/turbopack-3cd4a09b290ecea9.js"
+])
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  distDir: 'docs',
   trailingSlash: true,
   // basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   // assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
